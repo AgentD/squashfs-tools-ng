1d81e68dd152ea73b98aa2d8edb7b3095e4ded6d
David Oberhollenzer <david.oberhollenzer@sigma-star.at>
2019-07-30 07:20:24 +0200

    Update print_version text
    This commit updates the text issued by print_version() to reflect in
    some way that the software contains contributions from co-authors.
    
    The original text was based on the sterotypical --version output of
    GNU coreutils programs. It may have to be rewritten eventually.
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

--- lib/util/print_version.c
+++ lib/util/print_version.c
@@ -18,12 +18,10 @@ extern char *__progname;
 
 static const char *version_string =
 "%s (%s) %s\n"
-"Copyright (c) 2019 David Oberhollenzer\n"
+"Copyright (c) 2019 David Oberhollenzer et al\n"
 "License " LICENSE_SHORT ": " LICENSE_LONG " <" LICENSE_URL ">.\n"
 "This is free software: you are free to change and redistribute it.\n"
-"There is NO WARRANTY, to the extent permitted by law.\n"
-"\n"
-"Written by David Oberhollenzer.\n";
+"There is NO WARRANTY, to the extent permitted by law.\n";
 
 void print_version(void)
 {
