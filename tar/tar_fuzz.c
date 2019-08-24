2d43d376d38ebf91d0ef6b8711bdc134aa1abdd6
David Oberhollenzer <david.oberhollenzer@sigma-star.at>
2019-08-24 21:26:44 +0200

    Add minimal test program for fuzzing the tar parser
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

---
+++ tar/tar_fuzz.c
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * tar_fuzz.c
+ *
+ * Copyright (C) 2019 David Oberhollenzer <goliath@infraroot.at>
+ */
+#include "config.h"
+
+#include "util.h"
+#include "tar.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	tar_header_decoded_t hdr;
+	int fd, ret;
+
+	if (argc != 2) {
+		fputs("usage: tar_fuzz <tarball>\n", stderr);
+		return EXIT_FAILURE;
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror(argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	for (;;) {
+		ret = read_header(fd, &hdr);
+		if (ret > 0)
+			break;
+		if (ret < 0)
+			goto fail;
+
+		ret = lseek(fd, hdr.sb.st_size, SEEK_CUR);
+
+		clear_header(&hdr);
+		if (ret)
+			goto fail;
+	}
+
+	close(fd);
+	return EXIT_SUCCESS;
+fail:
+	close(fd);
+	return EXIT_FAILURE;
+}
