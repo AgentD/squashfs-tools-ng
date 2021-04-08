65955d6c48319124c12c7901fb0c0cf8a466a1a1
David Oberhollenzer <david.oberhollenzer@sigma-star.at>
2021-04-08 12:06:03 +0200

    Fix: libsquashfs: visibillity of mempool functions
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

--- include/mempool.h
+++ include/mempool.h
@@ -8,6 +8,7 @@
 #define MEMPOOL_H
 
 #include "compat.h"
+#include "sqfs/predef.h"
 
 typedef struct mem_pool_t mem_pool_t;
 
@@ -15,13 +16,13 @@ typedef struct mem_pool_t mem_pool_t;
 extern "C" {
 #endif
 
-mem_pool_t *mem_pool_create(size_t obj_size);
+SQFS_INTERNAL mem_pool_t *mem_pool_create(size_t obj_size);
 
-void mem_pool_destroy(mem_pool_t *mem);
+SQFS_INTERNAL void mem_pool_destroy(mem_pool_t *mem);
 
-void *mem_pool_allocate(mem_pool_t *mem);
+SQFS_INTERNAL void *mem_pool_allocate(mem_pool_t *mem);
 
-void mem_pool_free(mem_pool_t *mem, void *ptr);
+SQFS_INTERNAL void mem_pool_free(mem_pool_t *mem, void *ptr);
 
 #ifdef __cplusplus
 }
